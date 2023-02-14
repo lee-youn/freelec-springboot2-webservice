3df8aa11f79f3583a51323055872ea5666e7eb3a
Lee Youn Jeong <5816654@naver.com>
2023-02-15 04:19:55 +0900

    5장 스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현

--- src/main/java/com/jojoldu/book/service/PostsService.java
+++ src/main/java/com/jojoldu/book/service/PostsService.java
@@ -1,4 +1,4 @@
-package com.jojoldu.book.service.posts;
+package com.jojoldu.book.service;
 
 import com.jojoldu.book.domain.posts.Posts;
 import com.jojoldu.book.domain.posts.PostsRepository;
--- src/main/java/com/jojoldu/book/web/IndexController.java
+++ src/main/java/com/jojoldu/book/web/IndexController.java
@@ -1,11 +1,13 @@
 package com.jojoldu.book.web;
 
-import com.jojoldu.book.service.posts.PostsService;
+import com.jojoldu.book.config.auth.LoginUser;
+import com.jojoldu.book.config.auth.dto.SessionUser;
+import com.jojoldu.book.service.PostsService;
 import com.jojoldu.book.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
@@ -13,9 +15,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
